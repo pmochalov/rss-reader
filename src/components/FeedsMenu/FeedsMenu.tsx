b6ad793b92b5ab5c63a7b54c9237c37f4fbd8b5f
pmochalov <52402489+pmochalov@users.noreply.github.com>
2025-03-21 12:11:45 +0300

    added form 'new feed'

--- src/components/FeedsMenu/FeedsMenu.tsx
+++ src/components/FeedsMenu/FeedsMenu.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useAppSelector } from "../../hooks";
 import s from "./FeedsMenu.module.css";
 
@@ -5,11 +6,40 @@ const FeedsMenu: React.FC = () => {
     const { items } = useAppSelector((state) => state.menufeeds);
 
     return (
-        <nav className={s.menu}>
-            {items.map((item) => (
-                <a href={`/${item.id}`}>{item.title}</a>
-            ))}
-        </nav>
+        <>
+            <nav className={s.menu}>
+                {items.map((item) => (
+                    <a href={`/${item.id}`}>{item.title}</a>
+                ))}
+            </nav>
+            <FeedAddForm />
+        </>
+    );
+};
+
+const FeedAddForm: React.FC = () => {
+    const [title, setTitle] = useState<string>("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("submit");
+    };
+
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const { value } = e.target;
+
+        setTitle(value);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type='text' title={title} onChange={handleChange} />
+            <button type='submit' disabled={title.trim().length === 0}>
+                Добавить
+            </button>
+        </form>
     );
 };
 
