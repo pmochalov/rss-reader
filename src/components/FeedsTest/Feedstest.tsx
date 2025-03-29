af50e554671bc05bee744cf253c24d61cce02e32
pmochalov <52402489+pmochalov@users.noreply.github.com>
2025-03-29 15:55:42 +0300

    added menu feed

---
+++ src/components/FeedsTest/Feedstest.tsx
@@ -0,0 +1,9 @@
+import { useAppSelector } from "../../hooks";
+
+const Feedstest: React.FC = () => {
+    // const { items } = useAppSelector((state) => state.sections);
+
+    return <div>Меню с потоками</div>;
+};
+
+export { Feedstest };
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,6 +4,7 @@ import s from "./Layout.module.css";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
 import { FeedsMenu } from "../FeedsMenu/FeedsMenu";
+import { Feedstest } from "../FeedsTest/Feedstest";
 
 const Layout = () => {
     return (
@@ -13,7 +14,9 @@ const Layout = () => {
             <div className={s.main__aside}>
                 <FeedsMenu />
             </div>
-            <div className={s.main__feedslist}></div>
+            <div className={s.main__feedslist}>
+                <Feedstest />
+            </div>
             <div className={s.main__outlet}>
                 <Outlet />
             </div>
